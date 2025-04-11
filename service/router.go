7ee99980d9b58ae5737fdf01395a8d5a4d02ca8f
Vincent Yang <missuo@pm.me>
2025-04-11 00:23:34 -0400

    Add root route to router for status check and project information

--- service/router.go
+++ service/router.go
@@ -8,9 +8,12 @@ import (
 
 func Router(cfg *Config) *gin.Engine {
 	router := gin.Default()
-
 	// Register routes
 	router.GET("/genPlist", GeneratePlist)
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "plistserver is running on Vercel. See https://github.com/missuo/vercel-plistserver for more information. \n\n Made with ❤️ by Vincent."})
+	})
 
 	// Catch all other routes
 	router.NoRoute(func(c *gin.Context) {
